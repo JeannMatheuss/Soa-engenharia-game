dce9c5a0bc86dff71b6264da111245bc15c708d9
Jean <jeanmatheus0612@gmail.com>
2026-03-03 14:17:11 -0300

    EngenhariaGame

---
+++ src/main/java/br/com/fiap/engenhariagame/EngenhariagameApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.engenhariagame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngenhariagameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngenhariagameApplication.class, args);
+	}
+
+}
